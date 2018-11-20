9b012f57a871b1f87c36192d7bcbdb99ec673bab
TrumanHeberle <trumanheberle@gmail.com>
2018-11-20 16:14:49 -0500

    Revert "Set up proper data gathering framework"
    This reverts commit e16b90d9c88a7bb28afbc16c46fa5e3988d97fd3.

--- src/main/python/rlbot/ai/context.py
+++ src/main/python/rlbot/ai/context.py
@@ -1,29 +1,10 @@
 from rlbot.ai.actions import ACTIONS
-import random, tensorflow as tf, toml, os
-
-DEFAULT_SETTINGS = {
-}
+import random, tensorflow
 
 class Context:
     def __init__(self, agent):
         self.agent = agent
-        self.settings = DEFAULT_SETTINGS
-        # get user defined variables
-        try:
-            self.settings = {**self.settings, **toml.load("./src/main/python/rlbot/ai/settings.toml")}
-        except Exception as e:
-            print("failed to load ai settings: " + str(e))
-        # finish setup
-        self.init()
-    def init(self):
-        """called when the context needs to be initialized."""
-        self.session = tf.Session(config=tf.ConfigProto(allow_soft_placement=True))
-    def retire(self):
-        """called when the context needs to be shut down."""
-        if not (self.session):
-            raise UserWarning("context may not have been initialized correctly")
-            return
-        self.session.close()
+        # TODO: add tensorflow session, model, and memory
+
     def make_decision(self, packet):
-        """called when the context is required to make a decision given an input packet."""
         random.choice(ACTIONS).set_state(self.agent.controller_state)
--- src/test/python/agents/ai_bot/bot.py
+++ src/test/python/agents/ai_bot/bot.py
@@ -1,80 +1,23 @@
 from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
 from rlbot.utils.structures.game_data_struct import GameTickPacket
-import math, time, random, toml, os
+import math, time, random
 
-DEFAULT_SETTINGS = {
-    "framerate": {
-        "spa": 250,
-        "lag": 0.25
-    }
-}
-
-class AIBotInfo:
-    def __init__(self):
-        # non-user defined variables
-        self.last_time = time.time()
-        self.time_elapsed = None
-        self.last_action = SimpleControllerState()
-        self.total_responses = 0
-        self.lost_responses = 0
-        self.last_active = False
-        # get user defined variables
-        settings = DEFAULT_SETTINGS
-        try:
-            settings = {**settings, **toml.load("./src/test/python/agents/ai_bot/settings.toml")}
-            settings["framerate"]["spa"] /= 1000
-        except Exception as e:
-            print("failed to load bot settings: " + str(e))
-        # set user defined variables
-        self.spa = settings["framerate"]["spa"]
-        self.lag = settings["framerate"]["lag"]
+APS = 15 # actions per second
+SPA = 1/APS
 
 class AIBot(BaseAgent):
     def initialize_agent(self):
-        """called on bot initialization."""
+        #This runs once before the bot starts up
         self.controller_state = SimpleControllerState()
-        self.info = AIBotInfo()
+        self.last_time = time.time()
+
     def change_state(self, packet):
-        """called to alter the controller state of the bot."""
         self.context.make_decision(packet)
-    def retire(self):
-        """called when the bot needs to be shut down."""
-        self.context.retire()
+
     def get_output(self, packet: GameTickPacket) -> SimpleControllerState:
-        """called every frame the bot experiences, handles the bot internally."""
-        elapsed = time.time() - self.info.last_time
-        # ensure match is in progress and in regulated action rates
-        if (packet.game_info.seconds_elapsed == self.info.time_elapsed and self.info.last_active):
-            # first paused frame
-            self.info.last_time = time.time()
-            self.info.last_active = False
-            self.controller_state = self.info.last_action
-        elif (packet.game_info.seconds_elapsed == self.info.time_elapsed):
-            # remaining paused frames
-            self.info.last_time = time.time()
-            self.controller_state = self.info.last_action
-        elif (not packet.game_info.is_round_active and self.info.last_active):
-            # first inactive frame (kickoff and replays)
-            self.info.last_time = time.time()
-            self.info.last_active = False
-            self.controller_state = self.info.last_action
-        elif (not packet.game_info.is_round_active):
-            # remaining inactive frames
-            self.info.last_time = time.time()
-            self.controller_state = self.info.last_action
-        elif (elapsed > self.info.spa and elapsed < self.info.spa * (1 + self.info.lag)):
-            # acceptable decision lag time
-            self.info.last_time = time.time()
-            self.info.last_active = True
+        # regulate action rate
+        if (time.time() - self.last_time > SPA):
             self.change_state(packet)
-        elif (elapsed > self.info.spa):
-            # unacceptable decision lag time
-            self.info.last_time = time.time()
-            self.info.last_active = True
-            self.controller_state = self.info.last_action
-            self.lost_responses += 1
-        # return bot control decision (controller state)
-        self.info.time_elapsed = packet.game_info.seconds_elapsed
-        self.info.last_action = self.controller_state
-        self.info.total_responses += 1
+            self.last_time = time.time()
+
         return self.controller_state
